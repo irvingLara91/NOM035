907f1014a6e7be227aaf8e4e9d900e7c0809cfdd
diana_marrufo <diana.marrufo@hiumanlab.com>
2021-08-19 19:39:39 -0500

    "validacion en carga de usuarios"

--- screens/settings/UsersUpdate.js
+++ screens/settings/UsersUpdate.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from "react";
+import { Alert, StyleSheet } from 'react-native';
 import {Box, Button, Heading, Center, FlatList, Text, View, ScrollView } from "native-base";
-import {connect} from "react-redux";
-import MainLayout from "../../layouts/MainLayout";
 import * as DocumentPicker from 'expo-document-picker';
 import * as FileSystem from 'expo-file-system';
 import XLSX from 'xlsx';
-import { Alert } from 'react-native';
-
-import { storeData, retrieveData } from "../../helpers/storage"
+import _ from 'lodash';
+import MainLayout from "../../layouts/MainLayout";
+import { storeData, retrieveData } from "../../helpers/storage";
 
 const UsersUpdate = () => {
 
@@ -15,10 +14,9 @@ const UsersUpdate = () => {
 
     const getUsers = async () =>{
         let users = await retrieveData("users");
-        if ( users ) {
-            console.log(users);
-            setDatatable(users)
-        }
+        if ( users && users.length > 0  ) {
+            setDatatable(users);
+        } 
     }
 
     useEffect(()=>{
@@ -45,8 +43,16 @@ const UsersUpdate = () => {
 
     const saveUsers = async( data ) => {
         try {
-            await Promise.all( data.map( validateUsers ) );
-            storeData("users", data );
+            await Promise.all( data.map( validateUsers ) ).then( repeated => {
+                if ( datatable && datatable.length > 0 ){
+                    let tempArray = _.difference(datatable, repeated);
+                    let newArray = _.orderBy([...tempArray, ...data], ['idParticipante'], ['asc']);
+                    storeData("users", newArray );
+                } else {
+                    let newArray = _.orderBy(data, ['idParticipante'], ['asc']);
+                    storeData("users", newArray );
+                }
+            });
             return true;
         } catch (error) {
             console.log(error);
@@ -56,7 +62,12 @@ const UsersUpdate = () => {
     const validateUsers = ( users ) => {
         return new Promise( ( resolve, reject ) => {
             if( users.hasOwnProperty('idParticipante') && users.hasOwnProperty('username') && users.hasOwnProperty('password') ) {
-                resolve(true);
+                if ( datatable && datatable.length > 0 ){
+                    let userExist = _.find( datatable, {'idParticipante': users.idParticipante} )
+                    userExist ? resolve(userExist) : resolve({});
+                } else {
+                    resolve({});
+                }
             } else {
                 reject(false);
             }
@@ -65,57 +76,107 @@ const UsersUpdate = () => {
 
     return (
         <MainLayout>
-            <View>
-                <Center>
+            <View style={ styles.container }>
+                <Center py={ 8 }>
                     <Box>
-                        <Heading style={{color:'black', marginTop:'20%'}} size="lg" mb={3}>
+                        <Heading style={{ color:'black' }} size="lg" mb={3}>
                             Actualizar BD de usuarios
                         </Heading>
                     </Box>
                     <Button size={'lg'} style={{marginTop:20}} onPress={() => pickDocument()}>Cargar archivo .xlsx</Button>
                 </Center>
-                {
-                    datatable ?
-                    datatable.length > 0 ?
-                    <ScrollView style={{ maxHeight: '60%', marginTop:40, borderBottomColor: '#707070', borderWidth: 1, fontSize: 14 }}>
-                        <Text style={styles.titleHeaderTable}>Num de personas: {datatable.length}</Text>
-                        <View style={{ display: 'flex', flexDirection: 'row', borderBottomColor: '#707070', borderWidth: 1, fontSize: 14 }}>
-                            <View style={{ width: 60, paddingLeft: 10, paddingRight: 10, borderRightWidth: 1, borderColor: '#707070' }}>
-                                <Text fontSize={22} textAlign="center">id</Text>
-                            </View>
-                            <View style={{ paddingLeft: 10, paddingRight: 10 }}>
-                                <Text fontSize={22} textAlign="center">username</Text>
-                            </View>
+                { 
+                datatable && datatable.length > 0 ?
+                <ScrollView flex={1}>
+                    <Text style={styles.titleHeaderTable}> Número de personas: {datatable.length} </Text>
+                    <View style={ styles.headerTable }>
+                        <View style={ styles.item1Table }>
+                            <Text fontSize={18} textAlign="center" fontWeight="bold">id</Text>
                         </View>
-                        <FlatList
-                            data={datatable}
-                            renderItem={({ item }) => (
-                                <View style={{ display: 'flex', flexDirection: 'row', borderBottomColor: '#707070', borderWidth: 1, fontSize: 14 }}>
-                                    <View style={{ width: 60, paddingLeft: 10, paddingRight: 10, borderRightWidth: 1, borderColor: '#707070' }}>
-                                        <Text fontSize={18} textAlign="center">{item.idParticipante}</Text>
-                                    </View>
-                                    <View style={{ color: "#000000", paddingLeft: 10, paddingRight: 10, }}>
-                                        <Text fontSize={18} textAlign="center">{item.username}</Text>
-                                    </View>
+                        <View style={ styles.item2Table }>
+                            <Text fontSize={18} textAlign="center" fontWeight="bold">Nombre</Text>
+                        </View>
+                        <View style={ styles.item3Table }>
+                            <Text fontSize={18} textAlign="center" fontWeight="bold">username</Text>
+                        </View>
+                    </View>
+                    <FlatList
+                        data={datatable}
+                        renderItem={({ item }) => (
+                            <View style={ styles.itemsTable }>
+                                <View style={ styles.item1Table }>
+                                    <Text fontSize={18} textAlign="center">{item.idParticipante}</Text>
                                 </View>
-                            )}
-                            keyExtractor={(item) => item.idParticipante }
-                            />
-                    </ScrollView> : <Text py={40} pyfontSize={22} textAlign="center">No existen usuarios</Text>
-                    : <Text py={40} fontSize={22} textAlign="center">No existen usuarios</Text>
-                    }
+                                <View style={ styles.item2Table }>
+                                    <Text fontSize={18} textAlign="center">{item.nombre}</Text>
+                                </View>
+                                <View style={ styles.item3Table }>
+                                    <Text fontSize={18} textAlign="center">{item.username}</Text>
+                                </View>
+                            </View>
+                        )}
+                        keyExtractor={(item) => item.idParticipante.toString() }
+                    />
+                </ScrollView> : <Text py={40} fontSize={22} textAlign="center">No existen usuarios</Text>
+                }
             </View>
         </MainLayout>
     )
 
 }
 
-const styles={
+const styles = StyleSheet.create({
+    container: { 
+        width: '100%',
+        height: '98%',
+        display: 'flex',
+        flexDirection: 'column',
+    }, 
     titleHeaderTable:{
-        fontSize:16,
-        textAlign:'center'
-    }
-}
+        fontSize: 18,
+        fontWeight: "bold",
+        textAlign:'center',
+        paddingVertical: 4,
+        borderColor: '#707070', 
+        borderWidth: 1, 
+        backgroundColor: '#add3ec'
+    },
+    headerTable: {
+        display: 'flex', 
+        flexDirection: 'row', 
+        borderBottomColor: '#707070', 
+        borderBottomWidth: 1, 
+        borderLeftWidth: 1, 
+        borderRightWidth: 1, 
+        backgroundColor: '#bfdff5'
+    },
+    itemsTable: { 
+        display: 'flex', 
+        flexDirection: 'row', 
+        borderBottomColor: '#707070', 
+        borderBottomWidth: 1,
+        borderLeftWidth: 1, 
+        borderRightWidth: 1, 
+        backgroundColor: '#e0ebf3'
+    },
+    item1Table: { 
+        width: '20%', 
+        paddingHorizontal: 8, 
+        borderRightWidth: 1, 
+        borderColor: '#707070'
+    },
+    item2Table: { 
+        width: '40%', 
+        paddingHorizontal: 8, 
+        borderRightWidth: 1, 
+        borderColor: '#707070'
+    },
+    item3Table: { 
+        width: '40%', 
+        paddingHorizontal: 8, 
+    },
+    
+})
 
 
 
