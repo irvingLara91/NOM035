426485a9633607e2a483770c6c3ee4ceb55cd215
Irving Lara <irving.lara@hiumanlab.com>
2021-08-27 15:30:35 -0500

    Etiquetas en los cuestionarios   1 y 3   y  visualización de las etiketas html en en componente

--- components/QuestionComponent.js
+++ components/QuestionComponent.js
@@ -134,14 +134,14 @@ const QuestionComponent = ({
                         <View style={{flexDirection:'row',padding:20}}>
                         <Button size={'lg'}
                             /*Cambiar colores del botón */
-                                _light={{borderColor:app.color, borderWidth:2,bg: app.fontColor, _text: {color: app.color}}}
+                                _light={{borderColor:app.color, borderWidth:2,bg: app.fontColor, _text: {color: app.color,fontFamily: 'Poligon_Bold'}}}
                                 _pressed={{borderColor:app.secondaryColor, borderWidth:0,bg: app.secondaryColor, _text: {color: app.color}}}
                             /***fin***/
                                 style={{marginRight: 20, width: '50%'}} isLoading={loading}
                                 onPress={() => setValue(1)}>Si</Button>
                         <Button size={'lg'}
                             /*Cambiar colores del botón */
-                                _light={{borderColor:app.color, borderWidth:2,bg: app.fontColor, _text: {color: app.color}}}
+                                _light={{borderColor:app.color, borderWidth:2,bg: app.fontColor, _text: {color: app.color,fontFamily: 'Poligon_Bold'}}}
                                 _pressed={{borderColor:app.secondaryColor, borderWidth:0,bg: app.secondaryColor, _text: {color: app.color}}}
                             /***fin***/
                                 isLoading={loading} style={{width: '50%'}} onPress={() => setValue(0)}>No</Button>
@@ -155,28 +155,28 @@ const QuestionComponent = ({
                 question.tipo === '4asc' || question.tipo === '4desc' ?
                     <VStack style={{paddingLeft: 30, paddingRight: 30}}>
                         <Button size={'lg'}
-                                _light={{borderColor:app.color, borderWidth:2,bg: app.fontColor, _text: {color: app.color}}}
+                                _light={{borderColor:app.color, borderWidth:2,bg: app.fontColor, _text: {color: app.color,fontFamily: 'Poligon_Bold'}}}
                                 _pressed={{borderColor:app.secondaryColor, borderWidth:0,bg: app.secondaryColor, _text: {color: app.color}}}
                                 style={{marginBottom: 20, width: '100%'}} isLoading={loading}
                                 onPress={() => setValue(question.tipo === '4desc' ? 0 : 4)}>Siempre</Button>
                         <Button size={'lg'}
-                                _light={{borderColor:app.color, borderWidth:2,bg: app.fontColor, _text: {color: app.color}}}
+                                _light={{borderColor:app.color, borderWidth:2,bg: app.fontColor, _text: {color: app.color,fontFamily: 'Poligon_Bold'}}}
                                 _pressed={{borderColor:app.secondaryColor, borderWidth:0,bg: app.secondaryColor, _text: {color: app.color}}}
                                 isLoading={loading}
                                 style={{marginBottom: 20, width: '100%'}}
                                 onPress={() => setValue(question.tipo === '4desc' ? 1 : 3)}>Casi siempre</Button>
                         <Button size={'lg'}
-                                _light={{borderColor:app.color, borderWidth:2,bg: app.fontColor, _text: {color: app.color}}}
+                                _light={{borderColor:app.color, borderWidth:2,bg: app.fontColor, _text: {color: app.color,fontFamily: 'Poligon_Bold'}}}
                                 _pressed={{borderColor:app.secondaryColor, borderWidth:0,bg: app.secondaryColor, _text: {color: app.color}}}
                                 isLoading={loading}
                                 style={{marginBottom: 20, width: '100%'}}
                                 onPress={() => setValue(question.tipo === '4desc' ? 2 : 2)}>Algunas veces</Button>
-                        <Button size={'lg'}     _light={{borderColor:app.color, borderWidth:2,bg: app.fontColor, _text: {color: app.color}}}
+                        <Button size={'lg'}     _light={{borderColor:app.color, borderWidth:2,bg: app.fontColor, _text: {color: app.color,fontFamily: 'Poligon_Bold'}}}
                                 _pressed={{borderColor:app.secondaryColor, borderWidth:0,bg: app.secondaryColor, _text: {color: app.color}}}
                                 isLoading={loading}
                                 style={{marginBottom: 20, width: '100%'}}
                                 onPress={() => setValue(question.tipo === '4desc' ? 3 : 1)}>Casi nunca</Button>
-                        <Button size={'lg'}     _light={{borderColor:app.color, borderWidth:2,bg: app.fontColor, _text: {color: app.color}}}
+                        <Button size={'lg'}     _light={{borderColor:app.color, borderWidth:2,bg: app.fontColor, _text: {color: app.color,fontFamily: 'Poligon_Bold'}}}
                                 _pressed={{borderColor:app.secondaryColor, borderWidth:0,bg: app.secondaryColor, _text: {color: app.color}}}
                                 isLoading={loading}
                                 style={{marginBottom: 20, width: '100%'}}
